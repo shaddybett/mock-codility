33624a5b3882cb01eabc8fa67a64f824ec7e0983
Shaddy <shadybett540@gmail.com>
2024-02-05 12:25:49 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-let people = ['Mary','Josee','Peter']
+let people = [{name: 'peter',age: 30},{name: 'Mary', age: 20},{name:'Clare',age: 25}]
+
+function old(people) {
+    let oldPeople = people.filter(people => people.age >= 25)
+    return oldPeople;
+}
+console.log(old(people))
