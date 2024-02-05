91b98bf96507ec5c560eb07e8d08b104e5b1b6c9
Shaddy <shadybett540@gmail.com>
2024-02-05 10:56:31 +0300

    wrote an algorithm in index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+function solution(A){
+    A.sort((a,b) => a-b);
+
+    let smallestPositive = 1;
+
+    for (let i = 0; i < A.length; i++){
+        if (A[i] <= 0){
+            continue;
+        }
+
+        if (A[i] === smallestPositive) {
+            smallestPositive++;
+        }
+        else{
+            return smallestPositive;
+        }
+    return smallestPositive;    
+    }
+};
+let number = A([1,3,6,4,1,2])
+console.log(number)
