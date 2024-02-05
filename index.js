3a1a77f3b3d0b06643316feb0adac03ebc462073
Shaddy <shadybett540@gmail.com>
2024-02-05 11:03:06 +0300

    added sample data

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-function solution(A){
-    A.sort((a,b) => a-b);
+console.log("hello");
+function solution(A) {
+  A.sort((a, b) => a - b);
 
-    let smallestPositive = 1;
+  let smallestPositive = 1;
 
-    for (let i = 0; i < A.length; i++){
-        if (A[i] <= 0){
-            continue;
-        }
-
-        if (A[i] === smallestPositive) {
-            smallestPositive++;
-        }
-        else{
-            return smallestPositive;
-        }
-    return smallestPositive;    
+  for (let i = 0; i < A.length; i++) {
+    if (A[i] <= 0) {
+      continue;
     }
-};
-let number = A([1,3,6,4,1,2])
-console.log(number)
+
+    if (A[i] === smallestPositive) {
+      smallestPositive++;
+    } else {
+      return smallestPositive;
+    }
+    return smallestPositive;
+  }
+}
+let number = A([1, 3, 6, 4, 1, 2]);
+console.log(number);
