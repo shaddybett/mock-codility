7bb9e9f0d1ea2bbfe870f84b36198b2ce6426857
Shaddy <shadybett540@gmail.com>
2024-02-06 22:14:50 +0300

    done

--- index.js
+++ index.js
@@ -101,6 +101,10 @@
 function findIndex(nums) {
     let indices = {}
     for (let i = 0;i < nums.length;i++) {
+        indices.push(nums.Index)
         
     }
+    return indices;
 }
+let numbers = findIndex([1,2,3,4,5])
+console.log(numbers)
