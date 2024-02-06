bb0529408f05ecf8a7e4e6b35816147236f8c263
Shaddy <shadybett540@gmail.com>
2024-02-06 21:31:08 +0300

    added another question

--- index.js
+++ index.js
@@ -54,37 +54,44 @@
 // let numbers = sortList([4,8,7,5,6,2,1,3])
 // console.log(numbers)
 
-function sortList(nums) {
-  const numbers = nums.map((num, index) => ({ num, index }));
-  numbers.sort((a, b) => a.num - b.num);
-  const sorted = numbers.map(item => item.num);
-  return sorted
-//   console.log(numbers);
-}
-let number = sortList([4,2,3,1]);
-console.log(number);
+// function sortList(nums) {
+//   const numbers = nums.map((num, index) => ({ num, index }));
+//   numbers.sort((a, b) => a.num - b.num);
+//   const sorted = numbers.map(item => item.num);
+//   return sorted
+// //   console.log(numbers);
+// }
+// let number = sortList([4,2,3,1]);
+// console.log(number);
 
-function sortedList(nums) {
-  const numSet = new Set();
-  const duplicates = [];
+// function sortedList(nums) {
+//   const numSet = new Set();
+//   const duplicates = [];
 
-  for (let i = 0; i < nums.length; i++) {
-    let num = nums[i];
-    if (numSet.has(num)) {
-      return num;
-    } else {
-      numSet.add(num);
-    }
+//   for (let i = 0; i < nums.length; i++) {
+//     let num = nums[i];
+//     if (numSet.has(num)) {
+//       return num;
+//     } else {
+//       numSet.add(num);
+//     }
     
-  }
-//   return duplicates.length;
-//   return { unique: Array.from(numSet), duplicates };
+//   }
+// //   return duplicates.length;
+// //   return { unique: Array.from(numSet), duplicates };
+// }
+// let numbers = sortedList([2, 4, 6, 8, 3, 6, 9, 4]);
+// console.log(numbers)
+
+
+
+
+
+// // function firstDup
+
+function sortList(head) {
+    const ordered = head.sort((a,b)=> a-b)
+    return ordered;
 }
-let numbers = sortedList([2, 4, 6, 8, 3, 6, 9, 4]);
-console.log(numbers)
-
-
-
-
-
-// function firstDup
+let number = sortList([4,2,1,3])
+console.log(number)
